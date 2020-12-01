ee2ca4c6e6386409c860e7b810ca40cf7cdd80ca
mollymoog <mmkover@gmail.com>
2020-12-01 15:02:01 -0500

    commit tree c39ccf746da31188c83dfae009425c8e39164b20

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = 19%5;
+
+let max = Math.max(3, 18, 20);
